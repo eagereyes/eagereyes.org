83eafddc91dbeb91e23328e0108517734e36e019
Robert Kosara <rkosara@me.com>
2023-07-23 17:53:46 -0700

    Add sidebar generator, not fully working yet

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,4 +1,5 @@
 // import { defineConfig } from 'vitepress'
+import { generateSidebar } from 'vitepress-sidebar';
 
 // https://vitepress.dev/reference/site-config
 export default {
@@ -15,19 +16,36 @@ export default {
 			{ text: 'Publications', link: '/publications/' }
 		],
 
-		sidebar: [
-			{
-				text: 'Publications',
-				items: [
-					{ text: 'Journal Papers', link: '/publications/' },
-					{ text: 'Conference Papers', link: '/publications/' },
-					{ text: 'Blog 2008', link: '/blog/2008/charts-and-metaphors'},
-					{ text: 'Blog 2008b', link: '/blog/2008/ny-times-the-best-and-worst-of-data-visualization'},
-					{ text: 'Blog 2008c', link: '/blog/2008/sad-state-of-infovis-contest'},
-					{ text: 'Blog 2023', link: '/blog/2023/course-on-data-vis-fundamentals-and-best-practices'},
-				]
-			}
-		],
+		sidebar: generateSidebar({
+			rootGroupText: 'Blog 2016',
+			scanStartPath: 'blog/2016',
+			collapseDepth: 2,
+			capitalizeFirst: true,
+			useTitleFromFrontmatter: true,
+			excludeFolders: ['node_modules']
+		}),
+		
+		// sidebar: [
+		// 	{
+		// 		text: 'Publications',
+		// 		items: [
+		// 			{ text: 'Journal Papers', link: '/publications/' },
+		// 			{ text: 'Conference Papers', link: '/publications/' },
+		// 			{ text: 'Blog 2008', link: '/blog/2008/charts-and-metaphors'},
+		// 			{ text: 'Blog 2008b', link: '/blog/2008/ny-times-the-best-and-worst-of-data-visualization'},
+		// 			{ text: 'Blog 2008c', link: '/blog/2008/sad-state-of-infovis-contest'},
+		// 			{ text: 'Blog 2023', link: '/blog/2023/course-on-data-vis-fundamentals-and-best-practices'},
+		// 		]
+		// 	},
+		// 	generateSidebar({
+		// 		documentRootPath: '/',
+		// 		rootGroupText: 'Blog 2016',
+		// 		scanStartPath: 'blog/2016',
+		// 		useTitleFromFrontmatter: true,
+		// 		collapseDepth: 2,
+		// 		hyphenToSpace: true
+		// 	})[0]
+		// ],
 
 		socialLinks: [
 			{ icon: 'github', link: 'https://github.com/vuejs/vitepress' },
@@ -41,11 +59,10 @@ export default {
 		outlineTitle: 'Contents'
 	},
 
-	// set page title for publications
-	async transformPageData(pageData) {
-		if (pageData.relativePath.startsWith('publications') && pageData.params) {
-			pageData.title = pageData.params.title
-		}
-	}
+	// // set page title for publications
+	// async transformPageData(pageData) {
+	// 	if (pageData.relativePath.startsWith('publications') && pageData.params) {
+	// 		pageData.title = pageData.params.title
+	// 	}
+	// }
 }
-
