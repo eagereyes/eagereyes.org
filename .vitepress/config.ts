241cdc0cb25432c0e8c79c3c1ba3caa09bbdc089
Robert Kosara <rkosara@me.com>
2023-08-10 21:11:44 -0700

    About and license pages

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -32,28 +32,11 @@ export default {
 				// useFolderLinkAsIndexPage: true,
 				excludeFolders: ['node_modules', 'publications']
 			}),
-		
-		// sidebar: [
-		// 	{
-		// 		text: 'Publications',
-		// 		items: [
-		// 			{ text: 'Journal Papers', link: '/publications/' },
-		// 			{ text: 'Conference Papers', link: '/publications/' },
-		// 			{ text: 'Blog 2008', link: '/blog/2008/charts-and-metaphors'},
-		// 			{ text: 'Blog 2008b', link: '/blog/2008/ny-times-the-best-and-worst-of-data-visualization'},
-		// 			{ text: 'Blog 2008c', link: '/blog/2008/sad-state-of-infovis-contest'},
-		// 			{ text: 'Blog 2023', link: '/blog/2023/course-on-data-vis-fundamentals-and-best-practices'},
-		// 		]
-		// 	},
-		// 	generateSidebar({
-		// 		documentRootPath: '/',
-		// 		rootGroupText: 'Blog 2016',
-		// 		scanStartPath: 'blog/2016',
-		// 		useTitleFromFrontmatter: true,
-		// 		collapseDepth: 2,
-		// 		hyphenToSpace: true
-		// 	})[0]
-		// ],
+
+		footer: {
+			message: 'All materials available under <a href="/license">CC-BY-SA</a>',
+			copyright: 'Copyright ©2006–present Robert Kosara'
+		},
 
 		socialLinks: [
 			{ icon: 'mastodon', link: 'https://mastodon.social/@eagereyes' },
@@ -96,11 +79,4 @@ export default {
 	sitemap: {
 		hostname: 'https://eagereyes.org'
 	},
-
-	// // set page title for publications
-	// async transformPageData(pageData) {
-	// 	if (pageData.relativePath.startsWith('publications') && pageData.params) {
-	// 		pageData.title = pageData.params.title
-	// 	}
-	// }
 }
---
+++ license.md
@@ -0,0 +1,15 @@
+---
+title: About
+prev: false
+next: false
+---
+
+# License
+
+All content on eagereyes is released under a <a href="https://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Share-Alike License</a>. That means you’re welcome to use anything you see here as long as you…
+
+- give me credit and
+- release your own content under the same terms.
+
+If you want to use materials for commercial purposes, I’m usually happy to grant permission. Just <a href="http://eagerstage.newmandi.com/contact/">ask</a>.
+
