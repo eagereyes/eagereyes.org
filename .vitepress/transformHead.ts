6578e8486cc3c99661b1d42133ff3af4cef6f447
Robert Kosara <rkosara@me.com>
2023-08-20 20:52:51 -0700

    Add more open graph tags

--- .vitepress/transformHead.ts
+++ .vitepress/transformHead.ts
@@ -7,11 +7,13 @@ import { type TransformContext } from 'vitepress';
 */
 export default async function transformHead(context: TransformContext) {
     const fm = context.pageData.frontmatter;
+    const url = 'https://eagereeyes.org/'+context.pageData.relativePath.slice(0, -3);
     let head = [
         [ 'meta', {property: 'og:title', content: fm.title } ],
         [ 'meta', {property: 'og:description', content: fm.description } ],
         [ 'meta', {property: 'og:image', content: fm.featuredImage} ],
-        [ 'meta', {property: 'og:url', content: 'https://eagereeyes.org/'+context.pageData.relativePath} ],
+        [ 'meta', {property: 'og:url', content: url } ],
+        [ 'meta', {property: 'og:type', content: 'article' } ],
     ];
 
     return head;
