d2a7269d55e1097f04ebae8af2b4922b5a333e81
Robert Kosara <rkosara@me.com>
2024-01-09 20:38:32 -0800

    Test signup form

---
+++ .vitepress/theme/ZohoSignupForm.vue
@@ -0,0 +1,2 @@
+<script src="https://campaigns.zoho.com/js/zc.iframe.js"></script>
+<iframe frameborder="0" id="iframewin" width="100%" height="100%" src="https://zcvmf-zgfm.maillist-manage.com/ua/Optin?od=11287ecc7b0fd5&zx=159db02&tD=110846cf4c117f3c1&sD=110846cf4c1181c94"></iframe>
---
+++ subscribe.md
@@ -0,0 +1,12 @@
+---
+title: How to follow or subscribe
+prev: false
+next: false
+---
+<script setup>
+    import ZohoSignupForm from '.vitepress/theme/ZohoSignupForm.vue';
+</script>
+
+# How to follow or subscribe
+
+<ZohoSignupForm />
