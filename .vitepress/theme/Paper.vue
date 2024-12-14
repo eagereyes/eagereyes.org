c41bc76a6531d582cbaa47c82fcda6a3a6c9cc5f
Robert Kosara <rkosara@me.com>
2024-12-14 14:28:44 -0800

    Formatting

--- .vitepress/theme/Paper.vue
+++ .vitepress/theme/Paper.vue
@@ -8,14 +8,14 @@ const year = props.bib._key.split(':')[2].substring(0, 4);
 
 <template>
 {{ bib.author.split(' and ').join(', ') }},
-    <strong v-if="bib._pdf && !list">{{ bib.title }}</strong>
-    <a v-else-if="list" :href="`${slug}`"><strong>{{ bib.title }}</strong></a>
-    <a v-else :href="`https://media.eagereyes.org/papers/${year}/${slug}.pdf`"  target="_blank"><strong>{{ bib.title }}</strong></a>
-    <span v-if="bib._type === 'inbook'">, in {{ bib.editor }}, <em>{{ bib.venue }}</em></span>
-    <em v-else>, {{ bib.venue }}</em>
-    <span v-if="bib.hasOwnProperty('volume')">, vol. {{ bib.volume }}</span>
-    <span v-if="bib.hasOwnProperty('number')">, no. {{ bib.number }}</span>
-    <span v-if="bib.hasOwnProperty('pages')">, pp. {{ bib.pages }}</span>
-    <span v-if="bib.hasOwnProperty('publisher')">, {{ bib.publisher }}</span>, {{ year }}.
-    <span v-if="bib.hasOwnProperty('doi') && !list"><a :href="`https://dx.doi.org/${bib.doi}`" target="_new">DOI: {{ bib.doi }}</a></span>
+<strong v-if="bib._pdf && !list">{{ bib.title }}</strong>
+<a v-else-if="list" :href="`${slug}`"><strong>{{ bib.title }}</strong></a>
+<a v-else :href="`https://media.eagereyes.org/papers/${year}/${slug}.pdf`"  target="_blank"><strong>{{ bib.title }}</strong></a>
+<span v-if="bib._type === 'inbook'">, in {{ bib.editor }}, <em>{{ bib.venue }}</em></span>
+<em v-else>, {{ bib.venue }}</em>
+<span v-if="bib.hasOwnProperty('volume')">, vol. {{ bib.volume }}</span>
+<span v-if="bib.hasOwnProperty('number')">, no. {{ bib.number }}</span>
+<span v-if="bib.hasOwnProperty('pages')">, pp. {{ bib.pages }}</span>
+<span v-if="bib.hasOwnProperty('publisher')">, {{ bib.publisher }}</span>, {{ year }}.
+<span v-if="bib.hasOwnProperty('doi') && !list"><a :href="`https://dx.doi.org/${bib.doi}`" target="_new">DOI: {{ bib.doi }}</a></span>
 </template>
