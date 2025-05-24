8ec7fd1a92ddc739c7400740e31dbf2ee316126e
Robert Kosara <rkosara@me.com>
2025-05-24 09:33:47 -0700

    Reduce makeIndexes to only create sidebar JSON file

--- makeIndexes.py
+++ makeIndexes.py
@@ -53,67 +53,6 @@ def scanDirectory(directory):
 
     return files
 
-def writeIndex(files, fileName, dirName):
-
-    files = sorted(files, key=lambda f: f['date'], reverse=True)
-
-    with open(fileName, 'w') as out:
-        out.write('---\n')
-        out.write('title: Index for %s\n' % dirName )
-        out.write('outline: false\n')
-        
-        # for Blog year indices, link between years!
-        parts = dirName.split(' ')
-        if parts[0] == 'Blog':
-            if int(parts[1])+1 in posts:
-                out.write('next:\n  text: Blog %d\n  link: /blog/%d/\n' % (int(parts[1])+1, int(parts[1])+1))
-            else:
-                out.write('next: false\n')
-            if int(parts[1])-1 in posts:
-                out.write('prev:\n  text: Blog %d\n  link: /blog/%d/\n' % (int(parts[1])-1, int(parts[1])-1))
-            else:
-                out.write('prev: false\n')
-        else:
-            out.write('prev: false\n')
-            out.write('next: false\n')
-
-        out.write('---\n\n')
-        out.write('# %s\n\n' % dirName)
-
-        for file in files:
-            out.write('### <a href="/%s">%s</a>\n' % (file['path'][:-3], file['title']))
-            if len(file['description']) > 0:
-                out.write('%s _<a href="/%s">Read more…</a>_\n\n' % (file['description'], file['path'][:-3]))
-
-def writeBlogIndex(fileName, kind, value, longName=None):
-
-    longName = longName or value
-
-    with open(fileName, 'w') as out:
-        out.write('---\n')
-        out.write('title: Index for %s\n' % longName )
-        out.write('outline: false\n')
-        
-        # for Blog year indices, link between years!
-        if kind == 'year':
-            if year+1 in posts:
-                out.write('next:\n  text: Blog %d\n  link: /blog/%d/\n' % (value+1, value+1))
-            else:
-                out.write('next: false\n')
-            if year-1 in posts:
-                out.write('prev:\n  text: Blog %d\n  link: /blog/%d/\n' % (value-1, value-1))
-            else:
-                out.write('prev: false\n')
-        else:
-            out.write('prev: false\n')
-            out.write('next: false\n')
-
-        out.write('---\n\n')
-        out.write('# %s\n\n' % longName)
-
-        out.write('<BlogIndex %s=%s />\n' % (kind, value))
-
-
 tags = {}
 posts = {}
 
@@ -121,12 +60,11 @@ with scandir('blog') as it:
     for entry in it:
         if entry.is_dir():
             dir = 'blog/'+entry.name
-            print(dir)
+            # print(dir)
             files = scanDirectory(dir)
             
 allPosts = []
 for year in posts:
-    writeBlogIndex('blog/%d/index.md' % year, 'year', year)
     allPosts += posts[year]
 
 sidebar = [];
@@ -147,31 +85,3 @@ with open('blog/index.md', 'w') as out:
 sidebar = [{'text': 'Blog', 'items': sidebar}]
 
 dump(sidebar, open('sidebar.json', 'w'))
-
-writeBlogIndex('tag/criticism.md', 'tag', 'criticism', 'Criticism')
-writeBlogIndex('tag/book-reviews.md', 'tag', 'book-reviews', 'Book Reviews')
-writeBlogIndex('tag/pie-charts.md', 'tag', 'pie-charts', 'Pie Charts')
-writeBlogIndex('tag/isotype.md', 'tag', 'isotype', 'ISOTYPE')
-writeBlogIndex('tag/influences.md', 'tag', 'influences', 'Lists of Influences')
-writeBlogIndex('tag/eagereyestv.md', 'tag', 'eagereyestv', 'EagerEyes Videos')
-writeBlogIndex('tag/peer-review.md', 'tag', 'peer-review', 'Peer Review')
-writeBlogIndex('tag/conference.md', 'tag', 'conference', 'Conference Reports')
-writeBlogIndex('tag/basics.md', 'tag', 'basics', 'Visualization Basics')
-writeBlogIndex('tag/journalism.md', 'tag', 'journalism', 'Journalism')
-writeBlogIndex('tag/meta.md', 'tag', 'meta', 'Meta/Site News')
-writeBlogIndex('tag/paper.md', 'tag', 'paper', 'Papers')
-
-allPosts = sorted(allPosts, key=lambda f: f['date'], reverse=True)
-
-for p in allPosts:
-    p['date'] = p['date'].strftime('%B %d, %Y')
-    # strip the .md extension
-    p['path'] = p['path'][:-3]
-
-dump(allPosts[:20], open('home-feed.json', 'w'))
-print('%d posts' % len(allPosts))
-
-zipScribbles = scanDirectory('zipscribble-maps')
-writeIndex(zipScribbles, 'tag/zipscribble.md', 'ZIPScribble Maps')
-writeIndex(zipScribbles, 'zipscribble-maps/index.md', 'ZIPScribble Maps')
-print(f'{len(zipScribbles)} ZIPScribbles')
