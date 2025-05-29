c30d8c9b1f3beadb3695273576d06c84992a3afa
Robert Kosara <rkosara@me.com>
2025-05-28 22:04:44 -0700

    Some CSS stuff, Raleway font

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -16,17 +16,20 @@
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
-			<li aria-current={page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
-			</li>
 			<li aria-current={page.url.pathname.startsWith('/blog') ? 'page' : undefined}>
 				<a href="/blog/">Blog</a>
 			</li>
-			<li aria-current={page.url.pathname.startsWith('/publications') ? 'page' : undefined}>
-				<a href="/publications/">Publications</a>
-			</li>
 			<li aria-current={page.url.pathname.startsWith('/video') ? 'page' : undefined}>
 				<a href="/video/">Videos</a>
+			</li>
+			<li aria-current={page.url.pathname.startsWith('/app') ? 'page' : undefined}>
+				<a href="/">Apps</a>
+			</li>
+			<li aria-current={page.url.pathname.startsWith('/publications') ? 'page' : undefined}>
+				<a href="/publications/">Papers</a>
+			</li>
+			<li aria-current={page.url.pathname.startsWith('/about') ? 'page' : undefined}>
+				<a href="/about/">About</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
